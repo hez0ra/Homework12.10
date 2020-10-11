2b39c41081b9ed2e6f76377fef17832b05c35687
hez0ra <72031643+hez0ra@users.noreply.github.com>
2020-10-11 17:19:55 +0300

    разность между суммой квадратов и квадратом суммы первых 100 натуральных чисел.

--- taskN5.js
+++ taskN5.js
@@ -1,8 +1,20 @@
-Task:
-for(let i = 1;i<Infinity;i++){
-     for(let j = 1; j<=20; j++){
-          if(i%j != 0) continue Task
-     }
-     alert(i)
-     break Task
+let x = prompt('Введите число',100)
+function sum(x) {
+     let y = 0
+      for(let i = 1; i <=x; i++){
+        y += i*i
+      }
+      return y
 }
+function square(x){
+     let y = 0
+     for(let i = 0; i <=x; i++){
+          
+          y += i
+     }
+     y = y**2
+     return y
+}
+let answ = square(x) - sum(x)
+alert(answ)
+console.log(answ,sum(x),square(x))
