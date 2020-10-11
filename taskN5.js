82b711ee8fda36ab377505188df65a244a34e189
hez0ra <72031643+hez0ra@users.noreply.github.com>
2020-10-11 17:22:44 +0300

    Меньшее число которое делится на целые числа от 1 до 20

--- taskN5.js
+++ taskN5.js
@@ -1,20 +1,8 @@
-let x = prompt('Введите число',100)
-function sum(x) {
-     let y = 0
-      for(let i = 1; i <=x; i++){
-        y += i*i
-      }
-      return y
-}
-function square(x){
-     let y = 0
-     for(let i = 0; i <=x; i++){
-          
-          y += i
+Task:
+for(let i = 1;i<Infinity;i++){
+     for(let j = 1; j<=20; j++){
+          if(i%j != 0) continue Task
      }
-     y = y**2
-     return y
+     alert(i)
+     break Task
 }
-let answ = square(x) - sum(x)
-alert(answ)
-console.log(answ,sum(x),square(x))
